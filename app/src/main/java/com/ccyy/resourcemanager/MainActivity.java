1b91c55efdf0c57750791b8d84afd4502d0ac5cd
杨辉 <sweventears@foxmail.com>
2018-06-12 11:07:36 +0800

    修改界面

--- app/src/main/java/com/ccyy/resourcemanager/MainActivity.java
+++ app/src/main/java/com/ccyy/resourcemanager/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ccyy.resourcemanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.ccyy.resourcemanager.music.MusicActivity;
+import com.ccyy.resourcemanager.photo.PhotoActivity;
+import com.ccyy.resourcemanager.text.TextActivity;
+import com.ccyy.resourcemanager.video.VideoActivity;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -22,15 +28,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,17 +77,27 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        //todo 事件触发
+        if (id == R.id.nav_photo) {
+            Intent intent=new Intent(this, PhotoActivity.class);
+            startActivity(intent);
+        }
+        else if (id == R.id.nav_music) {
+            Intent intent=new Intent(this, MusicActivity.class);
+            startActivity(intent);
+        }
+        else if (id == R.id.nav_video) {
+            Intent intent=new Intent(this, VideoActivity.class);
+            startActivity(intent);
+        }
+        else if (id == R.id.nav_text) {
+            Intent intent=new Intent(this, TextActivity.class);
+            startActivity(intent);
+        }
+        else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        }
+        else if (id == R.id.nav_send) {
 
         }
 
