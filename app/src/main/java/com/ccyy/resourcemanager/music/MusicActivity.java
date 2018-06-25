59f66ef9ee529af0978c23783d71d0e40c8c77db
陈洪天 <1809615182@qq.com>
2018-06-25 22:12:32 +0800

    更新音乐播放器

--- app/src/main/java/com/ccyy/resourcemanager/music/MusicActivity.java
+++ app/src/main/java/com/ccyy/resourcemanager/music/MusicActivity.java
@@ -1,27 +1,55 @@
 package com.ccyy.resourcemanager.music;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.media.MediaPlayer;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.ccyy.resourcemanager.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicActivity extends AppCompatActivity {
 
-    private MediaPlayer myPlayer;
-    private TextView musicName;
-    private SeekBar myBar;
-    private Button play_pauseButton;
-    private Boolean isPlay;
+    private ArrayList<String> musicList;
+    private ArrayList<String> nameList;
+    private RecyclerView mRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.music_list);
+
+        mRecycler = findViewById(R.id.music_recycler);
+
+        musicList = new ArrayList();
+        nameList = new ArrayList();
+        String[] projection = {
+                MediaStore.Audio.Media._ID,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DISPLAY_NAME,
+                MediaStore.Audio.Media.SIZE
+        };
+
+        Cursor cursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, null, null, null);
+        while (cursor.moveToNext()) {
+            musicList.add(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA)));
+            nameList.add(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME)));
+        }
+
+        mRecycler.addItemDecoration(new DividerItemDecoration(this, 1));
+        mRecycler.setLayoutManager(new LinearLayoutManager(this));
+        MusicAdapter mAdapter = new MusicAdapter(this, musicList, nameList);
+        mRecycler.setAdapter(mAdapter);
 
     }
 }
