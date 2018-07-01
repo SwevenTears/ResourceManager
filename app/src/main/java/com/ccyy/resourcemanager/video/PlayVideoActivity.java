c46b627ef6f880e8007706278d25f7546a3e8a92
陈丹 <747672106@qq.com>
2018-07-01 22:17:38 +0800

    视频播放界面的布局修改，新增视频播放完成自动退出

--- app/src/main/java/com/ccyy/resourcemanager/video/PlayVideoActivity.java
+++ app/src/main/java/com/ccyy/resourcemanager/video/PlayVideoActivity.java
@@ -3,21 +3,22 @@ package com.ccyy.resourcemanager.video;
 
 import android.content.Context;
 import android.content.Intent;
-import android.drm.DrmStore;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.AttributeSet;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 import com.ccyy.resourcemanager.R;
+import com.ccyy.resourcemanager.tools.T;
 
 
 public class PlayVideoActivity extends AppCompatActivity{
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +33,17 @@ public class PlayVideoActivity extends AppCompatActivity{
         videoView.setMediaController(new MediaController(this));
         videoView.start();
 //        this.requestWindowFeature(Window.f);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //videoView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                finish();
+            }
+        });
+
 
     }
 
